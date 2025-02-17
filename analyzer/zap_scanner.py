28cac27c712b059f10d7392421d241de9d5adff9
Dingyao0 <82993127+Dingyao0@users.noreply.github.com>
2025-02-17 21:20:25 +0800

    Configure scanner to scan for injection and Broken Access Control only

--- analyzer/zap_scanner.py
+++ analyzer/zap_scanner.py
@@ -7,6 +7,56 @@ import streamlit as st
 load_dotenv()
 
 
+def configure_active_scanners(zap):
+    zap.ascan.disable_all_scanners()
+
+    # Enable Injection and Broken Access Control related scanners
+    injection_ids = [
+        "40018",
+        "40019",
+        "40020",
+        "40021",
+        "40022",
+        "40024",
+        "40027",
+        "90019",
+        "90020",
+        "90021",
+        "90035",
+        "90036",
+        "40003",
+        "90017",
+        "90029",
+        "40012",
+        "40014",
+        "40026",
+        "40016",
+        "40017",
+    ]  # IDs for Injection
+    bac_ids = ["6", "10104", "40035", "0", "40028"]  # IDs for Broken Access Control
+
+    st.write("Setting up Injection and Broken Access Control scanners...")
+
+    # Enabling Injection and BAC scanners
+    for scanner_id in injection_ids + bac_ids:
+        zap.ascan.set_scanner_alert_threshold(
+            scanner_id, "MEDIUM"
+        )  # Adjust threshold as needed
+        zap.ascan.set_scanner_attack_strength(
+            scanner_id, "HIGH"
+        )  # Set strength for more aggressive checks
+
+    st.write(
+        "Configured ZAP to scan only for Injection and Broken Access Control issues."
+    )
+
+
+def disable_passive_scanners(zap):
+    zap.pscan.disable_all_scanners()  # Disable all passive scanners
+    # Clear all existing alerts
+    zap.core.delete_all_alerts()
+
+
 def scan_url(target, scope):
     # Initialize ZAP API
     api_key = os.getenv("ZAP_API_KEY")
@@ -16,8 +66,11 @@ def scan_url(target, scope):
 
     zap = ZAPv2(
         proxies={"http": "http://localhost:8081", "https": "http://localhost:8081"},
-        apikey=os.getenv("ZAP_API_KEY"),
+        apikey=api_key,
     )
+
+    # disable_passive_scanners(zap)
+    configure_active_scanners(zap)
 
     # Page Only: Limit Spider to only the given page (no children)
     if scope == "Page Only":
@@ -39,6 +92,9 @@ def scan_url(target, scope):
             st.error(f"Error starting Spider scan: {e}")
             return  # Stop if Spider scan fails
 
+    # Wait for Spider scan to complete
+    while int(zap.spider.status()) < 100:
+        time.sleep(2)
     st.write("Spider scan completed. Starting Active scan.")
 
     try:
